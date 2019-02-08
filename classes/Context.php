72d31dec927544147a7eb472b7b7325955dabc7c
Markus Hitter <mah@jump-ing.de>
2019-02-08 22:29:37 +0100

    PHP classes: update/add license header.
    No functional change.

--- classes/Context.php
+++ classes/Context.php
@@ -1,32 +1,26 @@
 <?php
 /**
- * 2007-2016 PrestaShop
+ * Copyright (C) 2017-2019 thirty bees
+ * Copyright (C) 2007-2016 PrestaShop SA
  *
- * thirty bees is an extension to the PrestaShop e-commerce software developed by PrestaShop SA
- * Copyright (C) 2017-2018 thirty bees
+ * thirty bees is an extension to the PrestaShop software by PrestaShop SA.
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/afl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@thirtybees.com so we can send you a copy immediately.
  *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://www.thirtybees.com for more information.
- *
  * @author    thirty bees <modules@thirtybees.com>
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2017-2018 thirty bees
+ * @copyright 2017-2019 thirty bees
  * @copyright 2007-2016 PrestaShop SA
  * @license   Academic Free License (AFL 3.0)
- *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
+ * PrestaShop is an internationally registered trademark of PrestaShop SA.
  */
 
 namespace PsOneSixMigrator;
